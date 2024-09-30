8305296eab84caf9ff52d4d423648aba0adeb1bf
Darren Song <dazzasong@gmail.com>
2024-09-30 16:37:22 +1000

    Added food, drinks and consumption

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,5 +1,52 @@
-export default function Food() {
+import { useState } from "react";
+
+export default function Food({ src, drink=false }) {
+  const [isConsumed, setIsConsumed] = useState(false);
+
+  const eat1 = new Audio("eat1.mp3");
+  const eat2 = new Audio("eat2.mp3");
+  const drink1 = new Audio("drink1.mp3");
+  const drink2 = new Audio("drink2.mp3");
+
+  const consume = () => {
+    setIsConsumed(true);
+    const randomSfx = Math.floor(Math.random() * 2);
+    if (drink) {
+      switch (randomSfx) {
+        case 0:
+          drink1.play();
+          break;
+        case 1:
+          drink2.play();
+          break;
+        default:
+          throw new Error("Invalid randomSfx!");
+      }
+    } else {
+      switch (randomSfx) {
+        case 0:
+          eat1.play();
+          break;
+        case 1:
+          eat2.play();
+          break;
+        default:
+          throw new Error("Invalid randomSfx!");
+      }
+    }
+  }
+
   return (
-    <div></div>
-  )
+    <div>
+      {!isConsumed && (
+        <img
+          src={src}
+          alt=""
+          width={240}
+          onClick={consume}
+          style={{ cursor: "grab" }}
+        />
+      )}
+    </div>
+  );
 }
