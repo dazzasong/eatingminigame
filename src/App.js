08038ceb69cc350de6049ce321b55f20a7129818
Darren Song <dazzasong@gmail.com>
2024-10-07 22:01:15 +1100

    Removed straw drink sfx and replaced w/ gulp drink sfx

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import TopBar from "./TopBar/TopBar";
 import Food from "./Food";
-import { Stack } from "@mui/material";
 
 function App() {
+
+
   return (
     <div>
-      <Stack direction="row">
-        <Food />
-        <Food />
-        <Food />
-      </Stack>
     </div>
   );
 }
