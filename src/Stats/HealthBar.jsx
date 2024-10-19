943ad0cb25eddb22666bc608f072ebd6482a5cb9
Darren Song <dazzasong@gmail.com>
2024-10-19 14:10:06 +1100

    Stat bar color changes depending on value

--- src/Stats/HealthBar.jsx
+++ src/Stats/HealthBar.jsx
@@ -1,11 +1,17 @@
 import { Box, LinearProgress, Stack, Typography } from "@mui/material";
 
 function HealthBar({ value }) {
+  let color = "success";
+
+  if (value >= 75) color = "success";
+  else if (value >= 25 && value < 75) color = "warning";
+  else color = "error";
+
   return (
     <Stack direction="row" alignItems="center" spacing={1}>
       <Typography>Health:</Typography>
       <Box position="relative" left={6} width={200}>
-        <LinearProgress variant="determinate" value={value} />
+        <LinearProgress variant="determinate" color={color} value={value} />
       </Box>
     </Stack>
   );
--- src/Stats/ThirstBar.jsx
+++ src/Stats/ThirstBar.jsx
@@ -1,11 +1,16 @@
 import { Box, LinearProgress, Stack, Typography } from "@mui/material";
 
 function ThirstBar({ value }) {
+  let color = "warning";
+
+  if (value >= 25) color = "warning";
+  else color = "error";
+
   return (
     <Stack direction="row" alignItems="center" spacing={1}>
       <Typography>Thirst:</Typography>
       <Box position="relative" left={12} width={200}>
-        <LinearProgress variant="determinate" value={value} />
+        <LinearProgress variant="determinate" color={color} value={value} />
       </Box>
     </Stack>
   );
